21e20fa7ad7a74f10cadda8c0c6c6736e5f56424
Richard Howell <richardh@uber.com>
2016-03-25 14:08:22 +0100

    import Foundation before using FOUNDATION_EXPORT

--- UberSignals/UberSignals.h
+++ UberSignals/UberSignals.h
@@ -22,6 +22,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
+
 //! Project version number for UberSignals.
 FOUNDATION_EXPORT double UberSignalsVersionNumber;
 
